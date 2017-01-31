96de052a47b5bf80bcdca9b2f02801a6b7d451ec
Helga <Helga@ASUSX450>
2017-01-31 15:05:59 -0800

    Add project files.

---
+++ asp_mvc/Models/EntityManager/DBEntities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace asp_mvc_2.Models.EntityManager
+{
+    internal class DBEntities
+    {
+        public object SYSUserProfiles { get; internal set; }
+        public object SYSUserRoles { get; internal set; }
+        public object SYSUsers { get; internal set; }
+
+        internal void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
